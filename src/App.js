be3fe9b0b994d8e51264c5946277ac84621c3594
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-09 01:58:49 +0300

    Update App.js to use Home page as the main landing route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import LanguageSelection from "./components/LanguageSelection";
 import ArabicPage from "./components/ArabicPage";
 import EnglishPage from "./components/EnglishPage";
 import NotFound from "./components/NotFound";
+import ErrorLinksPage from "./react_Errors/ErrorLinksPage";
+import UseRoutesErrorPage from "./react_Errors/UseRoutesError/UseRoutesError";
+import FutureFlagWarningPage from "./react_Errors/FutureFlagWarningPage/FutureFlagWarningPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LanguageSelection />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/language-selection" element={<LanguageSelection />} />
         <Route path="/ar" element={<ArabicPage />} />
         <Route path="/en" element={<EnglishPage />} />
+        <Route path="/errors">
+          <Route index element={<ErrorLinksPage />} />
+          <Route path="use-routes-error" element={<UseRoutesErrorPage />} />
+          <Route
+            path="future-flag-warning"
+            element={<FutureFlagWarningPage />}
+          />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
